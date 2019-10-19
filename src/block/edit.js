0c0584acd0aeadb6fcd80a6241b009cf6dab8867
Mithun Biswas <bhoot.biswas@gmail.com>
2019-10-19 21:30:41 +0600

    Add edit.js

---
+++ src/block/edit.js
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import {
+	isUndefined,
+	pickBy
+} from 'lodash';
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import { Component, RawHTML } from '@wordpress/element';
+import {
+	PanelBody,
+	Placeholder,
+	QueryControls,
+	RangeControl,
+	Spinner,
+	ToggleControl,
+	Toolbar,
+	RadioControl,
+} from '@wordpress/components';
+import apiFetch from '@wordpress/api-fetch';
+import { addQueryArgs } from '@wordpress/url';
+import { __ } from '@wordpress/i18n';
+import { dateI18n, format, __experimentalGetSettings } from '@wordpress/date';
+import {
+	InspectorControls,
+	BlockControls,
+} from '@wordpress/block-editor';
+import { withSelect } from '@wordpress/data';
+
+/**
+ * Module Constants
+ */
+const CATEGORIES_LIST_QUERY = {
+	per_page: -1,
+};
+const MAX_POSTS_COLUMNS = 6;
